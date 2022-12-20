4fda7ef48c051a1d36cea051febf706c75aad65d
sasanek12 <sasanek12@gmail.com>
2022-12-20 01:30:43 +0100

    something... works?

--- js/grafik.js
+++ js/grafik.js
@@ -1,8 +1,21 @@
+var mode = "schedule-work"
+$( "#work" ).click(function() {
+    mode = "schedule-work"
+});
+$( "#call" ).click(function() {
+    mode = "schedule-call"
+});
+$( "#unavailable" ).click(function() {
+    mode = "schedule-unavailable"
+});
+$( "#holiday" ).click(function() {
+    mode = "schedule-holiday"
+});
 $(document).ready( function() {
     $('#grafik').click( function(event) {
         var target = $(event.target);
         $td = target.closest('td');
-        $td.addClass('greenBg');
+        $td.addClass(mode);
 
 
 
