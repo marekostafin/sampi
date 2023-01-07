bffbbdff7e840190e5f09cc853f87e311ba54957
Marek Ostafin <marek.ostafin16@gmail.com>
2023-01-07 22:37:28 +0100

    Część pierwsza uzupełniania pracowników

--- js/employee-data.js
+++ js/employee-data.js
@@ -3,49 +3,49 @@ var employeeData = [
         first_name: "Odeta",
         last_name: "Brosz",
         email: "obrosz@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "brosz-odeta"
     },
     {
         first_name: "Agata",
         last_name: "Gałęza",
         email: "agata.galeza@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "galeza-agata"
     },
     {
         first_name: "Kowalski",
         last_name: "Janusz",
         email: "janusz.kowalski@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "kowalski-janusz"
     },
     {
         first_name: "Jan",
         last_name: "Kowalski",
         email: "jan.kowalski@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "kowalski-jan"
     },
     {
         first_name: "Adam",
         last_name: "Nowak",
         email: "adam.nowak@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "nowak-adam"
     },
     {
         first_name: "Michał",
         last_name: "Szkot",
         email: "michal.szkot@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "szkot-michal"
     },
     {
         first_name: "Andrzej",
         last_name: "Wyż",
         email: "andrzej.wyz@dundermifflin.com",
-        location: "Kraków",
+        location: "Warszawa",
         link: "wyz-andrzej"
     }
 ]
