3bb931bd098a3d44b3a488830e37a40804bbf350
Marek Ostafin <marek.ostafin16@gmail.com>
2023-01-07 15:41:13 +0100

    Niedziałające ładowanie wiadomości i dodawanie nowych - następny krok: użyć localStorage

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,5 @@
 async function loadChatData() {
-    const res = await fetch('/sampi/js/chat-data.csv');
+    const res = await fetch('/sampi/js/chat-data.txt');
     let file = await res.text();
     const lines = file.split("\n");
     return lines;
@@ -26,7 +26,6 @@ function getPerson(link) {
 async function loadOpenChats() {
     let messageBox = document.getElementById("chat-list");
     let chatData = await loadChatData();
-    console.log(chatData);
     for (let chat of chatData) {
         let link = chat.split(";")[0];
         let person = await getPerson(link);
@@ -56,4 +55,82 @@ async function loadOpenChats() {
         `;
         messageBox.appendChild(a);
     }
+}
+
+async function loadCurrentChat(person) {
+    let messageBox = document.getElementById("current-chat");
+    let chatData = await getChatWith(person);
+    let personData = await getPerson(person);
+    const currentlyLoggedIn = "kowalski-jan";
+    console.log(chatData)
+    for (let i = 1; i < chatData.length; i+=2) {
+        if (chatData[i] === person) {
+        //     Sender Message
+            let avatarLink = "/sampi/style/img/avatar-" + person + ".png";
+            let div = document.createElement("div");
+            div.classList.add("media");
+            div.classList.add("w-50");
+            div.classList.add("mb-3");
+            div.innerHTML = `
+                <img src="${avatarLink}" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-3">
+                    <div class="bg-light  py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-muted">${chatData[i+1]}</p>
+                    </div>
+                    <p class="small text-muted">12:00 PM | Aug 13</p> <!--TODO -->
+                </div>
+            `
+            messageBox.appendChild(div);
+        } else if (chatData[i] === currentlyLoggedIn) {
+        //     Receiver Message
+            let div = document.createElement("div");
+            div.classList.add("media");
+            div.classList.add("w-50");
+            div.classList.add("ml-auto");
+            div.classList.add("mb-3");
+            div.innerHTML = `
+                <div class="media-body">
+                    <div class="color-message  py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-white">${chatData[i+1]}</p>
+                    </div>
+                    <p class="small text-muted">12:00 PM | Aug 13</p> <!--TODO -->
+                </div>
+            `
+            messageBox.appendChild(div);
+        } else {
+            console.error("Encountered unexpected message author id")
+        }
+    }
+}
+
+window.onkeyup = keyup;
+var inputTextValue;
+function keyup(e) {
+    inputTextValue = e.target.value;
+    if (e.keyCode == 13) {
+        newMessage(inputTextValue);
+    }
+}
+async function newMessage(message, currentChat) {
+    const fs = require("fs");
+    const lines = await loadChatData();
+    fs.truncate('/path/to/file', 0, function(){console.log('done')})
+    for (const currPerson of lines) {
+        var data = currPerson.split(";");
+        if (data[0] == currentChat) {
+            let writeData = currPerson.substring(0, currPerson.length-1)
+                + ";" + currentlyLoggedIn + ";" + message + "\n";
+            console.log(writeData);
+            fs.appendFile('/sampi/js/chat-data.txt', writeData, (err) => {
+                if (err) throw err;
+            })
+        } else {
+            let writeData = currPerson;
+            fs.appendFile('/sampi/js/chat-data.txt', writeData, (err) => {
+                if (err) throw err;
+            })
+        }
+    }
+    loadOpenChats();
+    loadCurrentChat(currentChat);
 }
