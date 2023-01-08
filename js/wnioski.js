9a671f1d96553fed4ab4408cdca526eb3405c0c1
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-01-08 22:38:42 +0100

    dodane strony z wnioskami

--- js/wnioski.js
+++ js/wnioski.js
@@ -1,29 +1,32 @@
 var data = [
     {
         name: "Zwrot kosztów delegacji",
+        link: "/sampi/pages/wnioski/zwrot-kosztów-delegacji.html",
         data: "01/01/2000",
-        send: true
+        send: false
     },
     {
         name: "Wniosek o urlop macierzyński",
+        link: "/sampi/pages/wnioski/wniosek-o-urlop-macierzynski.html",
         data: "01/01/2000",
         send: false
     },
     {
         name: "Zgłoszenie członka rodziny do ubezpieczenia",
-        data: "01/01/2000",
-        send: true
-    },
-    {
-        name: "Oświadczenie o wysokości podstawy ZUS",
-        data: "01/01/2000",
-        send: true
-    },
-    {
-        name: "Oświadczenie o zapoznaniu się z regulaminem pracy",
+        link: "/sampi/pages/wnioski/zgłoszenie-członka-rodziny-do-ubezpieczenia.html",
         data: "01/01/2000",
         send: false
     },
+    // {
+    //     name: "Oświadczenie o wysokości podstawy ZUS",
+    //     data: "01/01/2000",
+    //     send: true
+    // },
+    // {
+    //     name: "Oświadczenie o zapoznaniu się z regulaminem pracy",
+    //     data: "01/01/2000",
+    //     send: false
+    // },
 ]
 
 function storeData() {
@@ -38,6 +41,17 @@ function onStart(){
     let szkolenia = sessionStorage.getItem("wnioski")
     if(szkolenia == null) {
         storeData()
+    }
+}
+
+function hideWnioski(value) {
+    let regex = value.toLowerCase()
+    for (const wniosek of data) {
+        let check = (wniosek.name.toLowerCase()).match(regex)
+
+        if(!check) {
+            document.getElementById(wniosek.name).style.display = "none"
+        }
     }
 }
 
@@ -59,10 +73,11 @@ function loadWnioski() {
         let buttondiv = document.createElement("div");
         let datediv = document.createElement("div");
         if(wniosek.send === false) {
-            let button = document.createElement("button")
+            let button = document.createElement("a")
             button.className = "btn btn-outline-dark mt-1 mb-1 col-9"
             button.id = index;
             button.innerText = "Wypełnij wniosek"
+            button.href = wniosek.link;
             buttondiv.appendChild(button)
         } else {
             let button = document.createElement("button")
@@ -90,7 +105,11 @@ function loadWnioski() {
         `;
         contener.appendChild(li)
         index++
+
     }
+
+    hideWnioski(document.getElementById("wyszukiwarka").value);
 }
 
 onStart()
+document.addEventListener("input", loadWnioski)
